601d64246928675a057df728158aaeb9b8af25c1
Hedonista17 <andreaynereapiza@gmail.com>
2023-02-21 19:46:17 +0000

    finalizado generador al recargar la pagina, falta por boton

--- src/app.js
+++ src/app.js
@@ -7,31 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   document.querySelector(".card").classList.add(crearPaloRandom());
-  document.querySelector(".card").innerHTML = crearNumeroRandom();
+  document.querySelector(".numero").innerHTML = crearNumeroRandom();
 };
 
 let crearNumeroRandom = () => {
-  let numero = Math.floor(Math.random() * listaPoker.length);
-  let listaPoker = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+  let numero = Math.floor(Math.random() * 13);
+  let listaPoker = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   return listaPoker[numero];
 };
 
 let crearPaloRandom = () => {
-  let numero = Math.floor(Math.random() * palos.length);
+  let numero = Math.floor(Math.random() * 4);
   let palos = ["picas", "diamantes", "corazones", "treboles"];
   return palos[numero];
 };
+
+document.querySelector("#boton").addEventListener("click", crearPaloRandom);
