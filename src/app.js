84d5a8ddcc463574c1d2ce06e56ad7ae5a56649e
Hedonista17 <andreaynereapiza@gmail.com>
2023-02-21 19:19:52 +0000

    primera realizacion

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(crearPaloRandom());
+  document.querySelector(".card").innerHTML = crearNumeroRandom();
+};
+
+let crearNumeroRandom = () => {
+  let numero = Math.floor(Math.random() * listaPoker.length);
+  let listaPoker = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  return listaPoker[numero];
+};
+
+let crearPaloRandom = () => {
+  let numero = Math.floor(Math.random() * palos.length);
+  let palos = ["picas", "diamantes", "corazones", "treboles"];
+  return palos[numero];
 };
